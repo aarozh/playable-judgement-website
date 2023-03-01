b17451ae5441ffa22089d4f2bbb4dfd91247d88d
aaronzh <aaronzh@umich.edu>
2023-02-28 21:28:29 -0500

    Update main.py functionality for user input error and checking room code and css

--- judgement/schema.sql
+++ judgement/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS lobby;
 DROP TABLE IF EXISTS players;
 
 CREATE TABLE lobby(
-  lobby_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  lobby_id INTEGER,
   deck_id VARCHAR(20) NOT NULL,
   deck_type VARCHAR(10) NOT NULL,
   cards VARCHAR(10) NOT NULL,
@@ -11,7 +11,8 @@ CREATE TABLE lobby(
   round_start INTEGER NOT NULL,
   cur_round INTEGER NOT NULL,
   player_count INTEGER NOT NULL,
-  dealer INTEGER NOT NULL
+  dealer INTEGER NOT NULL,
+  PRIMARY KEY (lobby_id)
 );
 
 CREATE TABLE players(
--- judgement/views/main.py
+++ judgement/views/main.py
@@ -1,10 +1,60 @@
-"""Main."""
+"""
+Judgement get and post route for main page.
+
+URLs include:
+/
+"""
 
 import flask
 import judgement
 from flask import Flask
 from flask import render_template
+import random
+import string
 
-@judgement.app.route("/")
+def code_exists(code):
+    connection = judgement.model.get_db()
+    existing_code = connection.execute(
+        "SELECT lobby_id"
+        "FROM lobby"
+        "WHERE lobby_id = ?",
+        (code)
+    )
+    if existing_code.fetchone() is not None:
+        return True
+    return False
+
+def generate_room_code():
+    # check if this code is already created
+    generated_code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    while code_exists(generated_code):
+        generated_code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    return generated_code
+
+@judgement.app.route("/", methods=["POST", "GET"])
 def main_page():
+    flask.session.clear()
+    if flask.request.method == "POST":
+        name = flask.request.form.get("name")
+        code = flask.request.form.get("code")
+        join = flask.request.form.get("join", False)
+        create = flask.request.form.get("create", False)
+
+        if not name:
+            return render_template('mainpage.html', error_name="Please enter a name.", code = code)
+        
+        if join != False and not code:
+            return render_template('mainpage.html', error_code="Please enter a code.", name = name)
+
+        room = code
+        if create != False:
+            room = generate_room_code()
+        elif not code_exists(code):
+            return render_template('mainpage.html', error_code="Room code does not exist.", name = name)    
+
+        flask.session["room"] = room
+        flask.session["name"] = name
+        return flask.redirect(flask.url_for("lobby"))
+
+
     return render_template('mainpage.html')
